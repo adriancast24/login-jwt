bec1c733e995d2d60b8ec516c75182145ee2eb8b
JesusAlbujas <jesusramirez35000@gmail.com>
2024-01-10 11:37:04 -0400

    ya sirve las peticion get falta que los datos sean dinamicos

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:autentificacion_jwt/main.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:convert';
 
 class AuthController{
@@ -9,21 +12,46 @@ class AuthController{
   TextEditingController password_controller = TextEditingController();
 
   Future loginUser() async{
-    const url = 'http://192.168.0.119:8090/api/auth/login';
+    const url = 'http://adempiere.erpcya.com:1174/api/security';
 
-    var response = await http.post(Uri.parse(url),
+    var response = await http.post(Uri.parse("$url/login"),
       headers: { "Content-Type": "application/json",},
       body: jsonEncode({
-        "usernameOrEmail":"test4",
-        "password":"test"
+        "user_name":"jAlbujas",
+        "user_pass":"jAlbujas"
       }));
     if(response.statusCode==200){
       var loginarr = json.decode(response.body);
-      print(response.statusCode);
       print(loginarr);
+      return (response.body);
+
     }else {
       print(response.statusCode);
       print("xd");
     }
   }
+}
+
+Future get() async {
+  String? token = await storage.read(key: "jwt");
+  print("prueba $token");
+  //print(Value);
+  final res = await http
+      .get(Uri.parse('http://adempiere.erpcya.com:1174/api/security/menu'),
+      headers: {"Authorization" : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMjIxMzY2IiwiQURfQ2xpZW50X0lEIjoxMDAwMDAwLCJBRF9PcmdfSUQiOjEwMDAwNTEsIkFEX1JvbGVfSUQiOjEwMDAwMDAsIkFEX1VzZXJfSUQiOjEwMDAwMTcsIk1fV2FyZWhvdXNlX0lEIjoxMDAwNDM2LCJBRF9MYW5ndWFnZSI6ImVuX1VTIiwiaWF0IjoxNzA0ODk2ODk3LCJleHAiOjE3MDQ5ODMyOTd9.EGXg6uSSOfJntIEWoUnAzHaYVJI1UAV09PWgGidu_DI"}
+
+  );
+  if(res.statusCode==200){
+    var loginarr = json.decode(res.body);
+    print(res.statusCode);
+    print(loginarr);
+    return (res.body);
+
+  }else {
+    print(res.statusCode);
+    print("xd");
+  }
+
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-
 import 'package:autentificacion_jwt/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert' show json, base64, ascii;
+import 'dart:convert' show ascii, base64, json, jsonEncode;
 
 const SERVER_IP = 'http://192.168.5.101:8080/api/security/';
 final storage = FlutterSecureStorage();
@@ -22,7 +21,7 @@ class MyHome extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Log In", style: TextStyle(fontSize: 25),),
+          Text("Sign In", style: TextStyle(fontSize: 25),),
           SizedBox(height: 25,),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25,),
@@ -47,9 +46,16 @@ class MyHome extends StatelessWidget {
           ),
           SizedBox(height: 25,),
 
-          ElevatedButton(onPressed: (){
-            authController.loginUser();
-          }, child: Text('iniciar sesion'))
+          ElevatedButton(onPressed: () async {
+            var jwt = await authController.loginUser();
+            var res = get();
+            if(jwt != null){
+              storage.write(key: "jwt", value: jwt);
+
+
+            }
+
+          }, child: Text('iniciar sesion')  )
         ],
       )
 
