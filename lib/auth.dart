46423ec645b2deca8e00f9704d81afe60e56c1cf
adriancast24 <adriano159357258456@gmail.com>
2024-01-11 12:43:45 -0400

    last changes

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,77 +1,29 @@
-import 'dart:async';
 import 'dart:io';
 
-import 'package:autentificacion_jwt/main.dart';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:convert';
-
-class token{
-  String tokenauth;
-  token({required this.tokenauth,
-
-});
-  factory token.fromJson(Map<String, dynamic> tokend){
-    return token(
-      tokenauth : tokend['tokenauth'],
-    );
-  }
-
-}
-
-
 
 class AuthController{
   TextEditingController username_controller = TextEditingController();
   TextEditingController password_controller = TextEditingController();
 
   Future loginUser() async{
-    const url = 'http://adempiere.erpcya.com:1174/api/security';
+    const url = 'http://192.168.0.119:8090/api/auth/login';
 
-    var response = await http.post(Uri.parse("$url/login"),
+    var response = await http.post(Uri.parse(url),
       headers: { "Content-Type": "application/json",},
       body: jsonEncode({
-        "user_name": username_controller.text,
-        "user_pass": password_controller.text
+        "usernameOrEmail":"test4",
+        "password":"test"
       }));
     if(response.statusCode==200){
       var loginarr = json.decode(response.body);
       print(response.statusCode);
       print(loginarr);
-      return (response.body);
-
     }else {
       print(response.statusCode);
-
+      print("xd");
     }
   }
-}
-
-Future get() async {
-  var jwt = await storage.read(key: "jwt");
-
-
-
-  //token.forEach((key, value) =>print("$key=>${value.runtimeType.toString}"));
-
-  final res = await http
-      .get(Uri.parse('http://adempiere.erpcya.com:1174/api/security/menu'),
-      headers: {"Authorization" : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMjIxMzY2IiwiQURfQ2xpZW50X0lEIjoxMDAwMDAwLCJBRF9PcmdfSUQiOjEwMDAwNTEsIkFEX1JvbGVfSUQiOjEwMDAwMDAsIkFEX1VzZXJfSUQiOjEwMDAwMTcsIk1fV2FyZWhvdXNlX0lEIjoxMDAwNDM2LCJBRF9MYW5ndWFnZSI6ImVuX1VTIiwiaWF0IjoxNzA0ODk2ODk3LCJleHAiOjE3MDQ5ODMyOTd9.EGXg6uSSOfJntIEWoUnAzHaYVJI1UAV09PWgGidu_DI"}
-
-  );
-  if(res.statusCode==200){
-    var loginarr = json.decode(res.body);
-    print(res.statusCode);
-    print(loginarr);
-    return (res.body);
-
-  }else {
-    print(res.statusCode);
-    print("xd");
-  }
-
-
-
 }
